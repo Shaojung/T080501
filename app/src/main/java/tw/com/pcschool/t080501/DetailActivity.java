83e98a8c8ca8614830df152f33c7ea6967fdf282
shaojung <shaojung@gmail.com>
2016-08-05 14:36:46 +0800

    更新完成

--- app/src/main/java/tw/com/pcschool/t080501/DetailActivity.java
+++ app/src/main/java/tw/com/pcschool/t080501/DetailActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
-
+    TextView tv;
+    EditText ed4, ed5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,13 +18,20 @@ public class DetailActivity extends AppCompatActivity {
         Intent it = getIntent();
         int pos = it.getIntExtra("pos", 0);
 
-        TextView tv = (TextView) findViewById(R.id.textView);
-        EditText ed4 = (EditText) findViewById(R.id.editText4);
-        EditText ed5 = (EditText) findViewById(R.id.editText5);
+        tv = (TextView) findViewById(R.id.textView);
+        ed4 = (EditText) findViewById(R.id.editText4);
+        ed5 = (EditText) findViewById(R.id.editText5);
 
         Student s = StudentDAOImpl.mylist.get(pos);
         tv.setText(s.name);
         ed4.setText(s.addr);
         ed5.setText(s.tel);
+    }
+
+    public void click_update(View v)
+    {
+        StudentDAOImpl dao = new StudentDAOImpl(DetailActivity.this);
+        Student s = new Student(tv.getText().toString(), ed4.getText().toString(), ed5.getText().toString());
+        dao.updateStudent(s);
     }
 }
