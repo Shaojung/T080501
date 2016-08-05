1ce90e67b8272e54a6617e9f795a7257b39e1c74
shaojung <shaojung@gmail.com>
2016-08-05 11:44:58 +0800

    已加入新增資料

---
+++ app/src/main/java/tw/com/pcschool/t080501/AddActivity.java
@@ -0,0 +1,30 @@
+package tw.com.pcschool.t080501;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+    public void click_add(View v)
+    {
+        EditText ed = (EditText) findViewById(R.id.editText);
+        EditText ed2 = (EditText) findViewById(R.id.editText2);
+        EditText ed3 = (EditText) findViewById(R.id.editText3);
+
+        String n = ed.getText().toString();
+        String a = ed2.getText().toString();
+        String p = ed3.getText().toString();
+
+        StudentDAOImpl dao = new StudentDAOImpl(AddActivity.this);
+        dao.addStudent(new Student(n, a, p));
+
+    }
+}
--- app/src/main/java/tw/com/pcschool/t080501/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t080501/MainActivity.java
@@ -1,7 +1,10 @@
 package tw.com.pcschool.t080501;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,25 +18,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         StudentDAOImpl dao = new StudentDAOImpl(MainActivity.this);
+        /*
         dao.addStudent(new Student("AA", "11", "11111"));
         dao.addStudent(new Student("BB", "22", "222"));
         dao.addStudent(new Student("CC", "33", "3333"));
+*/
 
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         ArrayList<String> disp = new ArrayList<>();
         for (Student s :StudentDAOImpl.mylist)
         {
             disp.add(s.name);
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                                MainActivity.this,
-                                android.R.layout.simple_list_item_1,
-                                disp
-                                );
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                disp
+        );
         ListView lv = (ListView) findViewById(R.id.listView);
         lv.setAdapter(adapter);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_add)
+        {
+            Intent it = new Intent(MainActivity.this, AddActivity.class);
+            startActivity(it);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
