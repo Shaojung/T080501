93a76392d6b1ea3f7275c99f3ce58ec0f28416a1
shaojung <shaojung@gmail.com>
2016-08-05 11:19:56 +0800

    加入畫面列表。

--- app/src/main/java/tw/com/pcschool/t080501/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t080501/MainActivity.java
@@ -2,6 +2,11 @@ package tw.com.pcschool.t080501;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
 
         StudentDAOImpl dao = new StudentDAOImpl(MainActivity.this);
         dao.addStudent(new Student("AA", "11", "11111"));
+        dao.addStudent(new Student("BB", "22", "222"));
+        dao.addStudent(new Student("CC", "33", "3333"));
+
+        ArrayList<String> disp = new ArrayList<>();
+        for (Student s :StudentDAOImpl.mylist)
+        {
+            disp.add(s.name);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                                MainActivity.this,
+                                android.R.layout.simple_list_item_1,
+                                disp
+                                );
+        ListView lv = (ListView) findViewById(R.id.listView);
+        lv.setAdapter(adapter);
+
+
+
     }
 }
