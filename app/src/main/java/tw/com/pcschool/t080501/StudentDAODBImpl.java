51000e599f52039cb4af23e07c921a0eae11145b
shaojung <shaojung@gmail.com>
2016-08-08 13:44:50 +0800

    改為資料庫寫法，先處理 DAO 的框架。

---
+++ app/src/main/java/tw/com/pcschool/t080501/StudentDAODBImpl.java
@@ -0,0 +1,28 @@
+package tw.com.pcschool.t080501;
+
+import java.util.List;
+
+/**
+ * Created by Student on 2016/8/8.
+ */
+public class StudentDAODBImpl implements StudentDAO {
+    @Override
+    public void addStudent(Student s) {
+
+    }
+
+    @Override
+    public void delStudent(Student s) {
+
+    }
+
+    @Override
+    public void updateStudent(Student s) {
+
+    }
+
+    @Override
+    public List getAllStudent() {
+        return null;
+    }
+}
